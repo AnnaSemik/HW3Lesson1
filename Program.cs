03f6241bc1a0e761de1030ec8a2b1601c2a38054
AnnaS <anna-c-91@mail.ru>
2022-12-20 21:09:05 +1200

    Решена третья задача домашнего задания

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите число");
+string num1=Console.ReadLine();
+int number1=int.Parse(num1);
+if (number1%2==0)
+Console.Write ("Число "+number1+" - четное");
+else Console.Write ("Число "+number1+" - нечетное");
